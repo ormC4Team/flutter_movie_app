b8575660a11ded3862a748f342a112cba9850bec
nala0897@naver.com <nala0897@naver.com>
2024-04-19 16:37:03 +0900

    modify:사용하지 않는 코드 정리

--- lib/data/data_source/now_playing_api.dart
+++ lib/data/data_source/now_playing_api.dart
@@ -7,8 +7,6 @@ import 'package:http/http.dart' as http;
 class NowPlayingApi {
   String baseUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key=';
 
-  // String? myApiKey = dotenv.env['API_KEY'];
-
   Future<List<Results>> getNowPlayingList() async {
     final response = await http.get(Uri.parse(
         '${baseUrl}${dotenv.env['API_KEY']}&language=ko-KR&page=1'));
--- lib/data/data_source/upcoming_api.dart
+++ lib/data/data_source/upcoming_api.dart
@@ -8,8 +8,6 @@ import '../dto/upcoming_dto.dart';
 class UpcomingApi {
   String baseUrl = 'https://api.themoviedb.org/3/movie/upcoming?api_key=';
 
-  // String? myApiKey = dotenv.env['API_KEY'];
-
   Future<List<Results>> getUpcomingList() async {
     final response = await http.get(Uri.parse(
         '${baseUrl}${dotenv.env['API_KEY']}&language=ko-KR&page=1'));
