c536eb300d12751fc16f3dcee1947de596ac799e
nala0897@naver.com <nala0897@naver.com>
2024-04-19 15:23:48 +0900

    feat: mapper 구현 중

---
+++ lib/data/mapper/now_playing_mapper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_movie_app/data/dto/now_playing_dto.dart';
+import 'package:flutter_movie_app/data/model/now_playing.dart';
+
+extension NowPlayingMapper on NowPlayingDto {
+  // NowPlaying toNowPlaying() {
+  //   return NowPlaying(
+  //     id: id,
+  //     isAdult: isAdult,
+  //     posterPath: posterPath,
+  //     genreIds: genreIds,
+  //     title: title,
+  //     overview: overview,
+  //     releaseDate: releaseDate,
+  //     voteAverage: voteAverage,
+  //   );
+  // }
+}
